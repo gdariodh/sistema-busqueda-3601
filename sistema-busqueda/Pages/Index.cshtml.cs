6dbad76802952c745898411ea0ef22055d47b2c5
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-08-23 20:00:20 -0400

    inicializando method post formulario

--- sistema-busqueda/Pages/Index.cshtml.cs
+++ sistema-busqueda/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace sistema_busqueda.Pages
 {
     public class IndexModel : PageModel
     {
+
+       [BindProperty]
+        public string Usuario { get; set; }
+        [Display(Name="Contraseña")]
+        [BindProperty]
+
+        public string Password { get; set; } 
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +29,12 @@ namespace sistema_busqueda.Pages
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            var nombreUsuario = this.Usuario;
+            var passwordUsuario = this.Password;
         }
     }
 }
